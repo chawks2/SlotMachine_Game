700afc5192a1a20586456bbc7f8e3a714a938cbd
GEST Precision <Mufasa@BATTLECAT-2.local>
2015-04-09 09:04:11 -0400

    Slot Machine - CGrect

---
+++ SlotMachine/Factory.swift
@@ -0,0 +1,82 @@
+//
+//  Factory.swift
+//  SlotMachine
+//
+//  Created by GEST Precision on 2/23/15.
+//  Copyright (c) 2015 GEST Precision. All rights reserved.
+//
+
+import UIKit
+class Factory {
+
+//class func - not need for anything else in the code other than class - save memory etc.
+    class func createSlots () -> [[Slot]] {
+        
+        let kNumberOfSlots = 3
+        let kNumberOfContainers = 3
+        var slots: [[Slot]] = []
+        for var containerNumber = 0; containerNumber < kNumberOfContainers; ++containerNumber {
+            var slotArray:[Slot] = []
+            
+            //slots = [ [slot1,slot2,slot3], [slot4,slot5,slot6], [slot7, slot8, slot9] ]
+            //mySlotArray = slot[0] / slot1 slot2 slot3
+            
+            for var slotNumber = 0; slotNumber < kNumberOfSlots; ++slotNumber {
+                
+                //var slot = Slot(value: 0, image: UIImage(named: ""), isRed: true)
+                
+                var slot = Factory.createSlot(slotArray)
+                
+                slotArray.append(slot)
+            }
+            slots.append(slotArray)
+        }
+        return slots
+    }
+    
+    class func createSlot (currentCards : [Slot]) -> Slot {
+        var currentCardValues:[Int] = []
+        for slot in currentCards {
+            currentCardValues.append(slot.value)
+        }
+        var randomNumber = Int(arc4random_uniform(UInt32(13)))
+        while contains(currentCardValues, randomNumber + 1) {
+            randomNumber = Int(arc4random_uniform(UInt32(13)))
+        }
+        var slot:Slot
+        switch randomNumber {
+        case 0:
+            slot = Slot(value: 1, image: UIImage(named: "Ace"), isRed: true)
+        case 1:
+            slot = Slot(value: 2, image: UIImage(named: "Two"), isRed: true)
+        case 2:
+            slot = Slot(value: 3, image: UIImage(named: "Three"), isRed: true)
+        case 3:
+            slot = Slot(value: 4, image: UIImage(named: "Four"), isRed: true)
+        case 4:
+            slot = Slot(value: 5, image: UIImage(named: "Five"), isRed: false)
+        case 5:
+            slot = Slot(value: 6, image: UIImage(named: "Six"), isRed: false)
+        case 6:
+            slot = Slot(value: 7, image: UIImage(named: "Seven"), isRed: true)
+        case 7:
+            slot = Slot(value: 8, image: UIImage(named: "Eight"), isRed: false)
+        case 8:
+            slot = Slot(value: 9, image: UIImage(named: "Nine"), isRed: false)
+        case 9:
+            slot = Slot(value: 10, image: UIImage(named: "Ten"), isRed: true)
+        case 10:
+            slot = Slot(value: 11, image: UIImage(named: "Jack"), isRed: false)
+        case 11:
+            slot = Slot(value: 12, image: UIImage(named: "Queen"), isRed: false)
+        case 12:
+            slot = Slot(value: 13, image: UIImage(named: "King"), isRed: true)
+        default:
+            slot = Slot(value: 0, image: UIImage(named: "Ace"), isRed: true)
+        }
+        return slot
+    }
+
+
+}
+
---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by GEST Precision on 2/23/15.
+//  Copyright (c) 2015 GEST Precision. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
